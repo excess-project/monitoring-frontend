9fe30b5111ff71d3bfd4b96377ddb6b43e6b835d
hpcfapix <hpcfapix@hlrs.de>
2015-11-20 16:47:06 +0100

    a axis at bottom now !

--- public/javascripts/Rickshaw.Fixtures.Number.js
+++ public/javascripts/Rickshaw.Fixtures.Number.js
@@ -25,7 +25,7 @@ Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
 //fangli
 Rickshaw.Fixtures.Number.formatSeconds = function(y) {
     var abs_y = Math.abs(y);
-    if (abs_y === 0)         { return '' }
+    if (abs_y === 0)         { return '0' }
     else                     { return y + "s"}
 };
 //fangli
