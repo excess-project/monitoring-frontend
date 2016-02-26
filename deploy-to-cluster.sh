5e0fd29aba11bf1a95462ed4cc888dcf23462a45
hpcfapix <hpcfapix@hlrs.de>
2016-02-26 13:03:36 +0100

    deploy add 2nd argument: REPO

--- deploy-to-cluster.sh
+++ deploy-to-cluster.sh
@@ -3,11 +3,11 @@
 echo "Deploying a new development version of the monitoring server to the EXCESS cluster"
 
 VERSION=$1
-
+REPO=$2
 #
 # set parameters to retrieve artifact from nexus
 #
-REPO=snapshots
+#REPO=snapshots
 GROUP=eu/excess-project
 ARTIFACT=monitoring-frontend
 VERSION=${VERSION}
